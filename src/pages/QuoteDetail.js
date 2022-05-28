45c2f488be9d9b3bbc44424cb0781b6512df4b1c
M17ggie <102402171+M17ggie@users.noreply.github.com>
2022-05-28 18:26:21 +0530

    Add files via upload

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,51 @@
+import { useParams } from "react-router-dom";
+import Comments from '../components/comments/Comments';
+import { Route } from "react-router-dom";
+import HighlightedQuote from '../components/quotes/HighlightedQuote'
+import { Link } from "react-router-dom";
+import { useRouteMatch } from "react-router-dom";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import { useEffect } from "react";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+
+const QuoteDetail = () => {
+
+    const match = useRouteMatch();
+    const params = useParams();
+    const { quoteId } = params;
+
+    const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
+
+    useEffect(() => {
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId])
+
+    if (status === 'pending') {
+        return <div className="centered"><LoadingSpinner /></div>
+    }
+
+    if (error) {
+        return <p className="centered">{error}</p>
+    }
+
+    if (!loadedQuote.text) {
+        return <p>No quote found!</p>
+    }
+
+    return <section>
+        <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+        <Route path={match.path} exact>
+            <div className="centered">
+                <Link className='btn--flat' to={`${match.url}/comments`}>Load Comments</Link>
+            </div>
+        </Route>
+        <Route path={`${match.path}}/comments`}>
+            <Comments />
+        </Route>
+
+    </section>
+
+};
+
+export default QuoteDetail;
